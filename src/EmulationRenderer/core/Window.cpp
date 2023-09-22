df633881984006792a3ef78ea5d9c00eea6134a9
type <type>
2023-09-22 02:25:12 -0700

    Updated window.h

--- src/EmulationRenderer/core/Window.cpp
+++ src/EmulationRenderer/core/Window.cpp
@@ -1,6 +1,44 @@
 #include <core/Window.h>
+#include <GLFW/glfw3.h>
 #include <iostream>
+#include <unistd.h>
+#include <stdio.h>
 
-void WindowSetup(){
-    std::cout << "WINDOW SETUP IS PRINTING!!!\n";
+Window::~Window(){
+    glfwTerminate();
 }
+
+void Window::create(std::string title, int width, int height){
+
+    // Checking if glfw is setup correctly
+    if(!glfwInit()){
+        puts("GLFW did not work, is the glfw setup correctly?!\n");
+        return;
+    }
+
+    // Creating window
+    window = glfwCreateWindow(width, height, title.c_str(), nullptr, nullptr);
+    glfwMakeContextCurrent(window);
+    if(!window){
+        puts("Window could not be created!\n");
+        return;
+    }
+
+    while(!glfwWindowShouldClose(window)){
+        
+        if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS){
+            windowState = true;
+            glfwTerminate();
+        }
+
+        refresh();
+    }
+
+    glfwTerminate();
+}
+
+void Window::refresh(){
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+}
+
