c3c8084dd3137eaa5d4180d6f7bde65703056242
type <type>
2023-09-22 02:24:56 -0700

    Updated window.h

--- include/EmulationRenderer/core/Window.h
+++ include/EmulationRenderer/core/Window.h
@@ -1,4 +1,26 @@
 #pragma once
-#include <iostream>
+#include <GLFW/glfw3.h>
+#include <common/WindowUtils.h>
 
-void WindowSetup();
+/**
+ * @brief 
+ * 
+ * Window
+ * Handles instantiating window creating, including window data
+ * 
+ */
+
+class Window{
+public:
+    Window() = default;
+    Window(const Window&&) = delete;
+    ~Window();
+
+    void create(std::string title, int width, int height); // creating the window.
+    void refresh();
+
+private:
+    WindowUtils* windowUtils;
+    GLFWwindow* window; // This is the actual window for the renderer (acts as the canvas)
+    bool windowState; // state if window is turned on/off.
+};
